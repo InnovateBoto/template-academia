ef9302c16f1a91bc76fd0f46498f50c030df0b91
Guilherme <guilhermebscontact@gmail.com>
2023-09-07 17:56:02 -0300

    feat: hof corrigido

--- src/hof/Bootstrap.tsx
+++ src/hof/Bootstrap.tsx
@@ -1,5 +1,12 @@
-function Bootstrap(Component: React.ReactNode) {
-  return <div className="px-4">{Component}</div>;
+// Bootstrap.js
+import React from "react";
+
+function Bootstrap(Component: React.FC) {
+  return (
+    <div className="px-4">
+      <Component />
+    </div>
+  );
 }
 
 export default Bootstrap;

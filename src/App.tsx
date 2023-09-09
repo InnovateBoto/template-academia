d057a5114039862b235f1251fd11deebb49ed0c2
nazarejose <josenazare1234@gmail.com>
2023-09-09 11:50:25 -0300

    section 3 concluída

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Section from "./components/Section";
+import Sectionthree from "./components/Sectionthree";
 
 function App() {
   return (
     <>
       <Header />
       <Section />
+      <Sectionthree />
       <Footer />
     </>
   );
---
+++ src/components/Sectionthree.tsx
@@ -0,0 +1,18 @@
+import Bootstrap from "../hoc/Bootstrap";
+
+function Sectionthree() {
+  return (
+    <div className="bg-[#FFB94B] h-auto w-full items-center py-4">
+      <h1 className="text-center font-semibold text-3xl text-gray-900">
+        Nosso Time
+      </h1>
+      <div className="flex justify-evenly py-6 sm:flex-row flex-col items-center gap-y-8">
+        <div className="p-20 py-24 bg-gray-700 rounded-lg"></div>
+        <div className="p-20 py-24 bg-gray-800 rounded-lg"></div>
+        <div className="p-20 py-24 bg-gray-900 rounded-lg"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Bootstrap(Sectionthree, "#1f1f1f");
